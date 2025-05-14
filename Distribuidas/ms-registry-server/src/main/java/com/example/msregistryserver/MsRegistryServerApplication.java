e9ed4c1f8a2eb7b9614d7558712bc6a280c1e68c
Hector <pacompiahectorrobert@gmail.com>
2025-05-14 17:11:43 -0500

    Descripción de los cambios realizados

--- Distribuidas/ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
+++ Distribuidas/ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
@@ -3,13 +3,15 @@ package com.example.msregistryserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class MsRegistryServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsRegistryServerApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(MsRegistryServerApplication.class, args);
+        String port = context.getEnvironment().getProperty("server.port");
+        System.out.println(">>> Puerto configurado en Spring Boot: " + port);
     }
-
 }
